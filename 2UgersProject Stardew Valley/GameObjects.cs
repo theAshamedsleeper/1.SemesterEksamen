af8b01e90d9967a9e176149a315ee572c9ab82c8
Sky1pigeon <112692970+Sky1pigeon@users.noreply.github.com>
2022-11-15 11:55:32 +0100

    rhh

---
+++ 2UgersProject Stardew Valley/GameObjects.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace _2UgersProject_Stardew_Valley
+{
+    public abstract class GameObjects
+    {
+        protected Texture2D[] sprite;
+        protected Texture2D[] barSprite;
+        protected Rectangle[] energyRecBar = new Rectangle[5];
+        protected Rectangle seedChestRectangle;
+        protected Vector2 position;
+        protected float scale;
+        protected float speed = 200f;
+        protected Vector2 velocity;
+
+        #region Character attributes
+        protected byte charSpriteIndex=0;
+        protected Texture2D[] charaset;
+        protected Vector2[] position1 = new Vector2[5];
+        protected int threshold;
+        protected Rectangle sourceRectangles;
+        #endregion
+        public GameObjects(Vector2 pos)
+        {
+            position = pos;
+        }
+        private Texture2D currentSprite
+        {
+            get
+            {
+                return charaset[charSpriteIndex];
+            }
+        } 
+        
+        private Vector2 spriteSize
+        {
+            get
+            {
+                return new Vector2(sourceRectangles.Width * scale, sourceRectangles.Height * scale);
+            }
+        }
+        public abstract void LoadContent(ContentManager content);
+        public abstract void Update(GameTime gameTime);
+        
+        protected void Move(GameTime gameTime)
+        {
+            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            position += ((velocity * speed) * deltaTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            //player
+            Vector2 origin = new Vector2(sourceRectangles.Width / 2, sourceRectangles.Height / 2);
+            spriteBatch.Draw(currentSprite,//what to draw
+                position,//place to draw it
+                sourceRectangles,//rectangle
+                Color.White,//color of player
+                0f, //Rotation of player in radianer
+                origin,//Orgin Point
+                scale,//How big is the player
+                SpriteEffects.None,//effects
+                0f);//Layer higher the number further back it is 
+            //draws the sprite sheet for debugging
+            #region Draw Food and energy
+            //draw background color for energyBar
+            spriteBatch.Draw(barSprite[1],
+                position1[2],
+                energyRecBar[1],
+                Color.White,
+                (float)Math.PI,
+                new Vector2(0,0),
+                scale,
+                SpriteEffects.None,
+                0.2f); 
+            //draw background color for foodbar
+            spriteBatch.Draw(barSprite[3],
+                position1[4],
+                energyRecBar[3],
+                Color.White,
+                (float)Math.PI,
+                new Vector2(0,0),
+                scale,
+                SpriteEffects.None,
+                0.2f); 
+            //draw in foodBar
+            spriteBatch.Draw(barSprite[2],
+                position1[3],
+                energyRecBar[2],
+                Color.White,
+                (float)Math.PI,
+                new Vector2(0,0),
+                scale,
+                SpriteEffects.None,
+                0f);
+
+            //draws in energy bar
+            spriteBatch.Draw(barSprite[0],
+                position1[1],
+                energyRecBar[0],
+                Color.White,
+                (float)Math.PI,
+                new Vector2(0,0),
+                scale,
+                SpriteEffects.None,
+                0f);
+            #endregion
+        }
+        public Rectangle GetCollisionBox
+        {
+            get
+            {
+                return new Rectangle
+                (
+                    (int)(position.X - spriteSize.X / 2),
+                    (int)(position.Y - spriteSize.Y / 2),
+                    (int)spriteSize.X,
+                    (int)spriteSize.Y
+                );
+            }
+        }
+        public bool IsColliding(GameObjects other)
+        {
+            if (this == other)
+            {
+                return false;
+            }
+            return GetCollisionBox.Intersects(other.GetCollisionBox);
+        }
+    }
+}
---
+++ 2UgersProject Stardew Valley/Plant_t.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace _2UgersProject_Stardew_Valley
+{
+    internal static class Plant_t
+{
+        private static List<int[]> plantys = new List<int[]>();
+        private static List<int[]> plant_r_s = new List<int[]>();
+        private static List<int> plant_r = new List<int>();
+        private static float scale = Terrain.t_scale;
+        private static float grow = 0; 
+        #region new plants
+        public static void New_Plant(float x, float y, int p)
+        {
+            // the  2 if statements are to make sure we don't make many plants the same place
+            if (are_there_plants())
+            {
+                float scale = Terrain.t_scale;
+                float y_1 = (((y / scale) - ((y / scale) % 32f)) / 32f);
+                float x_1 = (((x / scale) - ((x / scale) % 32f)) / 32f);
+                int x_i = (int)x_1;
+                int y_i = (int)y_1; 
+                int[] pla = new int[5];
+                pla[0] = x_i;
+                pla[1] = y_i;
+                // plant growth
+                pla[2] = 1;
+                // plant wetness
+                pla[3] = 300;
+                //plant type
+                pla[4] = p;
+                plantys.Add(pla);
+            }
+            else
+            {
+                if (check(x, y) == -1)
+                {
+                    float scale = Terrain.t_scale;
+                    float y_1 = (((y / scale) - ((y / scale) % 32f)) / 32f);
+                    float x_1 = (((x / scale) - ((x / scale) % 32f)) / 32f);
+                    int x_i = (int)x_1;
+                    int y_i = (int)y_1; 
+                    int[] pla = new int[4];
+                    pla[0] = x_i;
+                    pla[1] = y_i;
+                    pla[2] = p;
+                    pla[3] = 300;
+                    plantys.Add(pla);
+                }
+            }
+        }
+        #endregion
+        #region updating
+        public static void update(float deltatime)
+        {
+            grow += deltatime;
+            // forgot i was gonna work with floats, and my plant list is ints. so this int will make more int like.
+            if (grow > 1)
+            {
+                for (int i = 0; i < plantys.Count; i++)
+                {
+                    //if below max growth
+                    if (plantys[i][2] < 1000)
+                    {
+                        // if wet
+                        if (plantys[i][3] > 0)
+                        {
+                            plantys[i][2] += 10;
+                            plantys[i][3] -= 5;
+                        }
+                    }
+                    else
+                    {
+                        plantys[i][3] -= 5;
+                    }
+                }
+                grow--;
+            }
+        }
+        #endregion
+        #region actions
+        /// <summary>
+        /// General checking function, given standard x and y coords, it will make those coords compatible, 
+        /// and then go through all plants to check if theres a plant at given location, was gonna make it return true, 
+        /// but giving the index of the plant proves more usefull, returns a -1 in case of false.
+        /// </summary>
+        /// <param name="x"> x coords </param>
+        /// <param name="y"> y coords </param>
+        /// <returns></returns>
+        private static int check(float x, float y)
+        {
+            x = (((x / scale) - ((x / scale) % 32f)) / 32f);
+            y = (((y / scale) - ((y / scale) % 32f)) / 32f);
+            for (int i = 0; i < plantys.Count; i++)
+            {
+                int[] pla = plantys[i];
+                if (x == pla[0])
+                {
+                    int[] pla_2 = plantys[i];
+                    if (y == pla_2[1])
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+        //removes plant at location
+        public static void plant_re(float x, float y)
+        {
+            int i = check(x, y);
+            if (i > -1)
+            {
+                plantys.RemoveAt(i);
+            }
+        }
+        // increases wetness of given plant
+        public static void plant_wet(float x, float y)
+        {
+            int i = check(x, y);
+            if (i > -1)
+            {
+                plantys[i][3] += 100;
+            }
+        }
+        #region checks
+        // plant check, returns growth
+        public static int Plant_Check_G(float x, float y)
+        {
+            int i = check(x, y);
+            if (i > -1)
+            {
+                return plantys[i][2];
+            }
+            return 0; 
+        }
+        // plant check, returns bool, if theres a plant at location
+        public static bool Plant_Check_b(float x, float y)
+        {
+            int i = check(x, y);
+            if (i > -1)
+            {
+                return true;
+            }
+            return false;
+        }
+        // plant check, returns wetness
+        public static int Plant_Check_wet(float x, float y)
+        {
+            int i = check(x, y);
+            if (i > -1)
+            {
+                return plantys[i][3];
+            }
+            return -1; ;
+        }
+        // plant check, returns bool, if there are plants
+        public static bool are_there_plants()
+        {
+            if (plantys.Count > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        #endregion
+        #endregion
+
+    }
+}
