b6ba6dc79c450b9733fb5623fb51ac083fa49c7a
theAshamedsleeper <thorbjorn114@gmail.com>
2022-11-28 10:19:38 +0100

    Revert "kjkk"
    This reverts commit 7e82ec00893337f8ed5eed3e913ba3d7f44d95da.

--- Miner/Miner/UiForInv/CraftingButton.cs
+++ Miner/Miner/UiForInv/CraftingButton.cs
@@ -100,18 +100,9 @@ namespace Miner.UiForInv
         public override void Update(GameTime gameTime)
         {
             mouse = Mouse.GetState();
-            UpgradeTab(gameTime);
-
-        }
-        private void CraftingTab()
-        {
-
-        }
-        private void UpgradeTab(GameTime gameTime)
-        {
             #region switch tabs
             //showing Craft tab
-            if (uiRectangles[0].Contains(mouse.X, mouse.Y) && mouse.LeftButton == ButtonState.Pressed)
+            if (uiRectangles[0].Contains(mouse.X,mouse.Y)&& mouse.LeftButton == ButtonState.Pressed)
             {
                 isCraftClicked = true;
                 isStatsClicked = false;
@@ -148,12 +139,12 @@ namespace Miner.UiForInv
             {
                 IsInvOpen = false;
             }
-            if (IsInvOpen == true && Keyboard.GetState().IsKeyDown(Keys.I) && closeDownShopTimer > 0.3f)
+            if(IsInvOpen == true &&Keyboard.GetState().IsKeyDown(Keys.I)&& closeDownShopTimer > 0.3f)
             {
                 IsInvOpen = false;
                 closeDownShopTimer = 0f;
             }
-            else if (IsInvOpen == false && Keyboard.GetState().IsKeyDown(Keys.I) && closeDownShopTimer > 0.3f)
+            else if (IsInvOpen == false && Keyboard.GetState().IsKeyDown(Keys.I)&& closeDownShopTimer > 0.3f)
             {
                 IsInvOpen = true;
                 closeDownShopTimer = 0f;
@@ -222,6 +213,15 @@ namespace Miner.UiForInv
             }
 
             #endregion
+
+        }
+        private void CraftingTab()
+        {
+
+        }
+        private void UpgradeTab()
+        {
+
         }
         private void ArtifactsTab()
         {
