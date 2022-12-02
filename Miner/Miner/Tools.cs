667daecead5ca18957b2579b184a28f8c9aa8ec7
BjarkeAR <91613897+BjarkeAR@users.noreply.github.com>
2022-12-02 13:34:27 +0100

    solar panel

--- Miner/Miner/Tools.cs
+++ Miner/Miner/Tools.cs
@@ -2,6 +2,9 @@
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Miner.UiForInv;
+using System;
+using Keyboard = Microsoft.Xna.Framework.Input.Keyboard;
+using Keys = Microsoft.Xna.Framework.Input.Keys;
 using SpriteBatch = Microsoft.Xna.Framework.Graphics.SpriteBatch;
 
 namespace Miner
@@ -14,18 +17,24 @@ namespace Miner
 
         public float Battery = 10;
         public float draintimer = 0;
+        public float gaintimer = 0;
         private int batteryFrame = 0;
 
 
         public int batteryMax = 10000;
+        public int solarPanelSize = 5000;
+        public int solarPanelCombined;
 
         public void LoadContent(ContentManager content)
         {
             batterySpritesheet = content.Load<Texture2D>(SPRITESHEET_BATTERY);
+            solarPanelCombined = batteryMax - solarPanelSize;
         }
 
+        
         public void Update(GameTime gameTime)
         {
+            //Battery upgrade
             if (UpgradeButton.Upgraded[7] == true)
             {
                 batteryMax = 14000;
@@ -43,15 +52,45 @@ namespace Miner
                 batteryMax = 11000;
             }
 
-        }
+            //Solar upgrade
+            if (UpgradeButton.Upgraded[11] == true)
+            {
+                solarPanelSize = 9000;
+            }
+            else if (UpgradeButton.Upgraded[10] == true)
+            {
+                solarPanelSize = 8000;
+            }
+            else if (UpgradeButton.Upgraded[9] == true)
+            {
+                solarPanelSize = 7000;
+            }
+            else if (UpgradeButton.Upgraded[8] == true)
+            {
+                solarPanelSize = 6000;
+            }
 
+        }
+        
         public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
         {
+            gaintimer += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Q) && batteryFrame > 0 && gaintimer > solarPanelCombined)
+            {
+
+                batteryFrame = batteryFrame - 30;
+                gaintimer = 0;
+                
+            }
+
+            
+
 
             draintimer += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
 
+            if (draintimer > batteryMax && batteryFrame <= 320 && !Keyboard.GetState().IsKeyDown(Keys.Q))
 
-            if (draintimer > batteryMax && batteryFrame <= 320)
             {
                 batteryFrame = batteryFrame + 30;
                 draintimer = 0;
